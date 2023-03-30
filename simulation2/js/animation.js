eb493c25a2fcb57122d10b1b11bf2a7de54bb374
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-30 22:29:18 +0530

    Simulation 2

--- simulation2/js/animation.js
+++ simulation2/js/animation.js
@@ -6,6 +6,6 @@ const weight = document.querySelector(".image");
 const play = document.querySelector(".play");
 
 play.addEventListener("click", () => {
-  root.style.setProperty("--rotate", "rotate(21deg)");
-  root.style.setProperty("--suiRotate", "rotate(-21deg)");
+  root.style.setProperty("--rotate", "rotate(20deg)");
+  root.style.setProperty("--suiRotate", "rotate(-20deg)");
 });
