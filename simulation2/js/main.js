0a7d7ead40ca597854b00d1d248d2a88c4af970a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-04-14 10:40:28 +0530

    Simulation 2

---
+++ simulation2/js/main.js
@@ -0,0 +1,54 @@
+function delayedNavigation() {
+   
+    var delayInMilliseconds = 1000; // 1 second
+
+   
+    setTimeout(function() {
+        window.location.href = "lab1.html";
+    }, delayInMilliseconds);
+  
+}
+var button = document.getElementById("mybtn1");
+button.addEventListener("click", delayedNavigation);
+
+
+function delayedNavigation2() {
+   
+    var delayInMilliseconds = 1000; // 1 second
+
+   
+    setTimeout(function() {
+        window.location.href = "lab2.html";
+    }, delayInMilliseconds);
+  
+}
+var button = document.getElementById("mybtn2");
+button.addEventListener("click", delayedNavigation2);
+
+function delayedNavigation3() {
+   
+    var delayInMilliseconds = 1000; // 1 second
+
+   
+    setTimeout(function() {
+        window.location.href = "lab3.html";
+    }, delayInMilliseconds);
+  
+}
+var button = document.getElementById("mybtn3");
+button.addEventListener("click", delayedNavigation3);
+
+function delayedNavigation4() {
+   
+    var delayInMilliseconds = 1000; // 1 second
+
+   
+    setTimeout(function() {
+        window.location.href = "lab4.html";
+    }, delayInMilliseconds);
+  
+}
+var button = document.getElementById("mybtn4");
+button.addEventListener("click", delayedNavigation4);
+
+
