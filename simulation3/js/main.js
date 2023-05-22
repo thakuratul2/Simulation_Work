df6fd76f7b4b3ec13522a6f36b239947cf17e6a9
thakuratul2 <pratapsinghatul111@gmail.com>
2023-05-22 20:30:15 +0530

    Simulation 3

---
+++ simulation3/js/main.js
@@ -0,0 +1,17 @@
+const gifImage = document.getElementById('gif_test');
+const playBtn = document.getElementById('motbut');
+
+
+
+playBtn.addEventListener('click', function() {
+  if (gifImage.classList.contains('paused')) {
+   
+    gifImage.classList.remove('paused');
+    gifImage.src = gifImage.src;
+    // playBtn.textContent = 'Pause';
+  } else {
+    
+    gifImage.classList.add('paused');
+   // playBtn.textContent = 'Play';
+  }
+});
